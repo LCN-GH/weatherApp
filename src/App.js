f93ed17f3c9038c86c3b349629e95cd91b90bf5a
LCN-GH <lewys.norton@gmail.com>
2021-06-25 11:45:46 +0100

    Finished weather app
    Shows min max temperatures and cloud cover when searching for a city or country.

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+const api = {
+  key: "f6dc0bdf42e9b086a5d84d590fd87837",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${city}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setCity("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
+  const newDate = (d) => {
+    let months = [
+      "January",
+      "Febuary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Enter a City or Country..."
+            onChange={(e) => setCity(e.target.value)}
+            value={city}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{newDate(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                Min: {Math.round(weather.main.temp_min)}°C - Max:{" "}
+                {Math.round(weather.main.temp_max)}°C
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather clouds">
+                Cloud cover: {weather.clouds.all}%
+              </div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default App;
